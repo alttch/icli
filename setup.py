27f71d8e034e5307f2bf8d0d15ecdeb1d8d4f506
divisor <div@altertech.com>
2019-09-29 02:07:30 +0200

    deps

--- setup.py
+++ setup.py
@@ -10,18 +10,15 @@ setuptools.setup(
     version=__version__,
     author='Altertech',
     author_email='div@altertech.com',
-    description=
-    'Format/create and print tables from lists of dicts',
+    description='Format/create and print tables from lists of dicts',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/alttch/rapidtables',
     packages=setuptools.find_packages(),
     license='MIT',
-    install_requires=[],
-    classifiers=(
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: User Interfaces'
-    ),
+    install_requires=['argcomplete', 'readline'],
+    classifiers=('Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Software Development :: Libraries',
+                 'Topic :: Software Development :: User Interfaces'),
 )
